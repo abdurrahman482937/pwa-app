bba1cd2163ff5e806b2c05017328577f82ac348e
Abdur Rahman <152638668+abdurrahman482937@users.noreply.github.com>
2025-04-21 21:53:38 +0600

    app

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Container, Navbar, Nav } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar bg="primary" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+    </div>
+  );
+}
+
+export default App;
